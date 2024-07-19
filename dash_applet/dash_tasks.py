34b8673a3bd19c9519a4258fc674433c77d6bbee
Kolliderrr <virus6409@gmail.com>
2024-07-19 18:02:33 +0500

    new_dash

--- dash_applet/dash_tasks.py
+++ dash_applet/dash_tasks.py
@@ -1,7 +1,7 @@
 import dash_mantine_components as dmc
 from dash import html
 import pandas as pd
-from typing import Union, Dict, List, Any
+from typing import Union, Dict, List, Any, Tuple
 
 
 class MyException(Exception):
@@ -40,7 +40,7 @@ def generate_table(content: Union[List[Dict[str, Any]], pd.DataFrame]) -> dmc.Ta
                             for key in element.keys()    
                         ]
                     )
-                for element in content
+                for element in content[1:]
             ]
             
             header = dmc.TableThead(
@@ -58,5 +58,42 @@ def generate_table(content: Union[List[Dict[str, Any]], pd.DataFrame]) -> dmc.Ta
             raise MyException('Неправильный формат данных для таблицы! См. описание')
     except MyException:
         return html.Div('Неправильный формат данных')
-        
 
+def generate_table_headers(columns: Union[List[str], tuple]) -> dmc.TableThead:
+    """Генератор заголовков таблицы dmc.Table
+
+    Args:
+        columns (Union[List[str], tuple]): Входящее значение [List] или [Tuple]
+ 
+    Returns:
+        dmc.TableThead: заголовки для dash_mantine_components.Table
+    """
+    return dmc.TableThead(
+        dmc.TableTr(
+                [
+                    dmc.TableTh(key)
+                    for key in columns
+                ]
+            )
+        )
+
+def generate_table_row(data: Union[Dict[List[Any]], Dict[Tuple[Any]]]) -> dmc.TableTr[dmc.TableTd]:
+    """_summary_
+
+    Args:
+        data (Union[Dict[List[Any]], Dict[Tuple[Any]]]): на входе - словарь с названием потребителя в ключе
+            и Union[List, Tuple] данных
+
+    Returns:
+        dmc.TableTr[dmc.TableTd]: Body таблицы
+    """
+    return [
+            dmc.TableTr(
+                [
+                    dmc.TableTd(el, id=f'{key}-{i}-cell')
+                    for i, el in elements
+                ],
+                id=f'{key}-row'
+            )
+        for key, elements in data.items()
+            ]
